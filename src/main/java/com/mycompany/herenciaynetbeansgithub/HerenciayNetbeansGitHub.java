364576e7c40d350305aac0c34a050e7d4d406bac
migue <pasosmiguel822018@gmail.com>
2025-10-01 09:38:43 -0500

    Segundo commit modificado

--- src/main/java/com/mycompany/herenciaynetbeansgithub/HerenciayNetbeansGitHub.java
+++ src/main/java/com/mycompany/herenciaynetbeansgithub/HerenciayNetbeansGitHub.java
@@ -12,5 +12,7 @@ public class HerenciayNetbeansGitHub {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
+         System.out.println("Cambio");
     }
 }
